7611d0b0c758abf159b049abb2bda07046399913
alantothe <rubenamalpartida@gmail.com>
2022-12-05 16:47:51 -0500

    login, register and dashboard routes working

--- src/App.js
+++ src/App.js
@@ -1,16 +1,32 @@
 
 import React from 'react';
+import {BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom'
 
 import './App.css';
 
+import Dashboard from './components/Dashboard/Dashboard';
+import LoginPage from './components/LoginPage/LoginPage';
+import RegisterPage from './components/RegisterPage/RegisterPage';
+
 
 function App() {
-  console.log("yo")
-  return (
-    <div className="App">
-    <h1>Hello</h1>
+  console.log("yo app")
 
-    </div>
+  return (
+<Router>
+        <Routes>
+          <Route exact path="/login" element={<LoginPage />}>
+
+          </Route>
+          <Route exact path="/register" element={<RegisterPage />}>
+
+          </Route>
+          <Route exact path="/dashboard" element={<Dashboard />}>
+
+          </Route>
+          <Route path="/" element={<Navigate to="dashboard" />} />
+        </Routes>
+      </Router>
   );
 }
 
