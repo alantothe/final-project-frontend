41bb0cf05215b95f1b18db623e737d845db1a601
alantothe <rubenamalpartida@gmail.com>
2022-12-08 11:51:39 -0500

    naviagte to register page from login page

--- src/components/LoginPage/LoginButton.js
+++ src/components/LoginPage/LoginButton.js
@@ -1,15 +1,24 @@
 import React from "react";
 import CustomButton from "../shared/SharedButton";
+import RedirectInfo from "../shared/RedirectInfo";
+import {useNavigate} from "react-router-dom"
 
 
 
 const LoginButton = ({ handleLogin, isFormValid }) => {
 
+  const navigate = useNavigate()
+
+  const handlePushToRegisterPage =() => {navigate('/register')}
+
+
+
+
 
 
 
   return (
-
+      <>
         <div>
           <CustomButton
             label="LOG IN"
@@ -17,7 +26,15 @@ const LoginButton = ({ handleLogin, isFormValid }) => {
             disabled={isFormValid}
             onClick={handleLogin}
           />
+
         </div>
+        <RedirectInfo
+        text="Need an account?"
+        redirectText={"Create an account"}
+        additionalStyles={{marginTop: "5px"}}
+        redirectHandler={ handlePushToRegisterPage}/>
+        </>
+
 
   );
 };
